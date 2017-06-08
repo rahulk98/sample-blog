80c350ee0dad90910697527069e8327f35949acd
rahulk98 <rahul.krishnan27@gmail.com>
2017-06-08 16:24:18 +0530

    Create settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yl-)yi=q&li4%$48ls3^+d)8ck#j92p&%yz=12l0tglgwe=3(%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rahulk98.pythonanywhere.com', '127.0.0.1']
 
